924c720af27dc03c2ac8e5dc690c50e7e29e196c
Dynasty <study_wc@163.com>
2024-10-21 22:01:02 +0800

    仓库创建

---
+++ AgentInfrastructure/src/main/java/com/agent/base/AgentInfrastructureApplication.java
@@ -0,0 +1,13 @@
+package com.agent.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentInfrastructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentInfrastructureApplication.class, args);
+	}
+
+}
